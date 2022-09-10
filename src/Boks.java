a9c1ae6ebf3c68c0c130eb8e8db9f4e2d3feaa06
muratA <muratriyum@gmail.com>
2022-09-10 11:14:23 +0300

    www.patika.dev

---
+++ src/Boks.java
@@ -0,0 +1,8 @@
+public class Boks {
+    public static void main(String[] args) {
+        Fighter marc = new Fighter("Marc" , 15 , 100, 90, 50);
+        Fighter alex = new Fighter("Alex" , 10 , 100, 100, 50);
+        Ring r = new Ring(marc,alex , 90 , 100);
+        r.run();
+    }
+}
